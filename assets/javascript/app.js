66f8f69879410e61a29e08157b12006e42355f04
Kevin Spies <kevin.spies@gmail.com>
2019-06-22 01:11:40 -0400

    url messing and figuring out

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,21 @@
 var topics = ["tennis", "coldplay", "pokemon"];
+
+$("#submitButton").on("click", function (event) {
+
+    event.preventDefault();
+    // var mykey = "KjWAKsFTOTtN5AD3PQQa4UA5Xj8nhPYH";
+    var mykey = "trilogy";
+    var topic = $("#search").val();//get user input
+    console.log(topic);
+    var topicURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + mykey + "&limit=5";
+
+
+    $.ajax({
+        url: topicURL,
+        method: "GET"
+    }).then(function (response) {
+
+
+
+    });
+})
