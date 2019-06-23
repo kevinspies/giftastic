a04102e799e67e251ca529f07b5dd17961e39135
Kevin Spies <kevin.spies@gmail.com>
2019-06-23 13:44:21 -0400

    added prevent default, so weird click refresh memory loss things don't happen or something

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,54 @@
-var topics = ["tennis", "coldplay", "pokemon"];
+var videogames = ["halo", "leagueoflegends", "pokemon", "monkeyball", "subnautica"];
 
-$("#submitButton").on("click", function (event) {
-
+// Event listener for all button elements
+$("button").on("click", function () {
     event.preventDefault();
-    // var mykey = "KjWAKsFTOTtN5AD3PQQa4UA5Xj8nhPYH";
-    var mykey = "trilogy";
-    var topic = $("#search").val();//get user input
-    console.log(topic);
-    var topicURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + mykey + "&limit=5";
+    // In this case, the "this" keyword refers to the button that was clicked
+    var videogame = $("#search").val();
+    console.log(videogame);
 
+    // Constructing a URL to search Giphy for the name of the person who said the quote
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        videogame + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
+    // Performing our AJAX GET request
     $.ajax({
-        url: topicURL,
+        url: queryURL,
         method: "GET"
-    }).then(function (response) {
+    })
+        // After the data comes back from the API
+        .then(function (response) {
+            // Storing an array of results in the results variable
+            var results = response.data;
 
+            // Looping over every result item
+            for (var i = 0; i < results.length; i++) {
 
+                // Only taking action if the photo has an appropriate rating
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                    // Creating a div for the gif
+                    var gifDiv = $("<div>");
 
-    });
-})
+                    // Storing the result item's rating
+                    var rating = results[i].rating;
+
+                    // Creating a paragraph tag with the result item's rating
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    // Creating an image tag
+                    var personImage = $("<img>");
+
+                    // Giving the image tag an src attribute of a proprty pulled off the
+                    // result item
+                    personImage.attr("src", results[i].images.fixed_height.url);
+
+                    // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                    gifDiv.append(p);
+                    gifDiv.append(personImage);
+
+                    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                    $("#gifs-appear-here").prepend(gifDiv);
+                }
+            }
+        });
+});
